7d5b569a5d53b638d782fb4d0cf4ebf1e2df8a55
Anta <ovaorange@yandex.ru>
2024-03-07 11:32:48 +0100

    resolve conflicts

--- uikit-sample/src/main/java/com/innoprog/android/uikitsample/MyBottomFragment.kt
+++ uikit-sample/src/main/java/com/innoprog/android/uikitsample/MyBottomFragment.kt
@@ -1,6 +1,17 @@
 package com.innoprog.android.uikitsample
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MyBottomFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_bottom, container, false)
+    }
 }
