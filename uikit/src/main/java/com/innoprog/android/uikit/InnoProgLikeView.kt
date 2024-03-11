d61906e6d67100bdc6f69d88314b342c0e2353f3
Vladislav <bigmag174@yandex.ru>
2024-03-11 19:52:42 +0500

    Убрал лишнее this@InnoProgLikeView

--- uikit/src/main/java/com/innoprog/android/uikit/InnoProgLikeView.kt
+++ uikit/src/main/java/com/innoprog/android/uikit/InnoProgLikeView.kt
@@ -23,19 +23,19 @@ class InnoProgLikeView @JvmOverloads constructor(
         inflate(context, R.layout.inno_prog_like_view, this)
         attrs?.applyStyleable(context, R.styleable.LikeCustomView) {
 
-            this@InnoProgLikeView.setBackgroundResource(R.drawable.rectangle_with_corners)
+            setBackgroundResource(R.drawable.rectangle_with_corners)
 
             if (getBoolean(R.styleable.LikeCustomView_liked, false)) {
 
                 likeIcon.setColorFilter(context.getColor(R.color.like_color))
-                this@InnoProgLikeView.background.setTint(context.getColor(R.color.like_background_color))
+                background.setTint(context.getColor(R.color.like_background_color))
                 countOfLikesTextView.setTextColor(context.getColor(R.color.like_color))
                 liked = true
             } else {
 
                 likeIcon.setColorFilter(context.getColor(R.color.text_secondary))
                 countOfLikesTextView.setTextColor(context.getColor(R.color.text_secondary))
-                this@InnoProgLikeView.background.setTint(context.getColor(R.color.background_secondary))
+                background.setTint(context.getColor(R.color.background_secondary))
                 liked = false
             }
 
@@ -53,7 +53,7 @@ class InnoProgLikeView @JvmOverloads constructor(
 
     private fun like() {
         likeIcon.setColorFilter(context.getColor(R.color.like_color))
-        this@InnoProgLikeView.background.setTint(context.getColor(R.color.like_background_color))
+        background.setTint(context.getColor(R.color.like_background_color))
         countOfLikesTextView.setTextColor(context.getColor(R.color.like_color))
 
         likeCount++
@@ -64,7 +64,7 @@ class InnoProgLikeView @JvmOverloads constructor(
     private fun unlike() {
         likeIcon.setColorFilter(context.getColor(R.color.text_secondary))
         countOfLikesTextView.setTextColor(context.getColor(R.color.text_secondary))
-        this@InnoProgLikeView.background.setTint(context.getColor(R.color.background_secondary))
+        background.setTint(context.getColor(R.color.background_secondary))
 
         likeCount--
         countOfLikesTextView.text = "$likeCount"
