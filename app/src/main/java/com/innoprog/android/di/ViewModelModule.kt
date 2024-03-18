5095dec7f75b97ffe24f39abc3262b4b41bdf34f
Bivis <lumenfan63@mail.ru>
2024-03-18 23:04:19 +0400

    убрал лишний импорт

--- app/src/main/java/com/innoprog/android/di/ViewModelModule.kt
+++ app/src/main/java/com/innoprog/android/di/ViewModelModule.kt
@@ -5,7 +5,6 @@ import com.innoprog.android.base.ViewModelSample
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
-import dagger.multibindings.StringKey
 
 @Module
 interface ViewModelModule {
