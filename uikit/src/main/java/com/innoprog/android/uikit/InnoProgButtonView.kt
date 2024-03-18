6daad2ce767b55f0f7d27660382373b0558041cc
Andrey_W <Uzzbagoyzza@yandex.ru>
2024-03-18 16:37:01 +0300

    Переделал InnoProgButtonView, теперь он не наследуеться от ButtonView а надуваеться из LinearLayout.

--- uikit/src/main/java/com/innoprog/android/uikit/InnoProgButtonView.kt
+++ uikit/src/main/java/com/innoprog/android/uikit/InnoProgButtonView.kt
@@ -2,41 +2,62 @@ package com.innoprog.android.uikit
 
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.view.Gravity
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 
 
 class InnoProgButtonView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
-) : androidx.appcompat.widget.AppCompatButton(context, attrs, defStyleAttr) {
+) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ButtonCustomView)
+    private val typedArray = context.obtainStyledAttributes(attrs, R.styleable.InnoProgButtonView)
     private val size =
-        ButtonSize.entries[typedArray.getInt(R.styleable.ButtonCustomView_buttonSize, 0)]
+        ButtonSize.entries[typedArray.getInt(R.styleable.InnoProgButtonView_buttonSize, 0)]
     private val type =
-        ButtonType.entries[typedArray.getInt(R.styleable.ButtonCustomView_buttonType, 0)]
+        ButtonType.entries[typedArray.getInt(R.styleable.InnoProgButtonView_buttonType, 0)]
+    private val state =
+        ButtonState.entries[typedArray.getInt(R.styleable.InnoProgButtonView_buttonState, 0)]
 
-    private val rightIcon: Drawable? =
-        typedArray.getDrawable(R.styleable.ButtonCustomView_rightIcon)
-    private val leftIcon: Drawable? = typedArray.getDrawable(R.styleable.ButtonCustomView_leftIcon)
+    private var text = typedArray.getText(R.styleable.InnoProgButtonView_text)
 
-    init {
-        setBackgroundResource(R.drawable.button_shape)
-        gravity = Gravity.CENTER
+    private var rightIconDrawable: Drawable? =
+        typedArray.getDrawable(R.styleable.InnoProgButtonView_rightIcon)
+    private var leftIconDrawable: Drawable? =
+        typedArray.getDrawable(R.styleable.InnoProgButtonView_leftIcon)
 
-        when (size) {
-            ButtonSize.LARGE -> setButtonSizeLarge()
-            ButtonSize.MEDIUM -> setButtonSizeMedium()
-            ButtonSize.SMALL -> setButtonSizeSmall()
-        }
-        typedArray.recycle()
+    private val textTV by lazy { findViewById<TextView>(R.id.text_button) }
+    private val rightIconIV by lazy { findViewById<ImageView>(R.id.right_icon) }
+    private val leftIconIV by lazy { findViewById<ImageView>(R.id.left_icon) }
+
+    enum class ButtonType {
+        PRIMARY, DEFAULT, FLAT
     }
 
+    enum class ButtonSize {
+        LARGE, MEDIUM, SMALL
+    }
+
+    enum class ButtonState {
+        ENABLED, DISABLED
+    }
+
+    init {
+
+        inflate(context, R.layout.inno_prog_button_view, this)
+        setBackgroundResource(R.drawable.button_shape)
+        setType()
+        setSize()
+        setIcons()
+        textTV.text = text
+        typedArray.recycle()
+
+    }
 
     private fun setType() {
         when (type) {
@@ -46,38 +67,38 @@ class InnoProgButtonView @JvmOverloads constructor(
         }
     }
 
+    private fun setSize() {
+        when (size) {
+            ButtonSize.LARGE -> setButtonSizeLarge()
+            ButtonSize.MEDIUM -> setButtonSizeMedium()
+            ButtonSize.SMALL -> setButtonSizeSmall()
+        }
+    }
+
     private fun setButtonPrimary() {
-        if (isEnabled) {
+        if (state == ButtonState.ENABLED) {
             background.setTint(context.getColor(R.color.accent_default))
-            setTextColor(context.getColor(R.color.text_primary))
+            textTV.setTextColor(context.getColor(R.color.text_primary))
         } else {
             background.setTint(context.getColor(R.color.background_secondary))
-            setTextColor(context.getColor(R.color.text_tertiary))
+            textTV.setTextColor(context.getColor(R.color.text_tertiary))
         }
     }
 
     private fun setButtonDefault() {
         background.setTint(context.getColor(R.color.text_field_fill))
-        setTextColor(context.getColor(R.color.accent_secondary))
-        alpha = if (isEnabled) {
-            1F
-        } else {
-            0.5F
-        }
+        textTV.setTextColor(context.getColor(R.color.accent_secondary))
+        alpha = if (state == ButtonState.ENABLED) 1F else 0.5F
     }
 
     private fun setButtonFlat() {
         setBackgroundColor(Color.TRANSPARENT)
-        setTextColor(context.getColor(R.color.accent_secondary))
-        alpha = if (isEnabled) {
-            1F
-        } else {
-            0.5F
-        }
+        textTV.setTextColor(context.getColor(R.color.accent_secondary))
+        alpha = if (state == ButtonState.ENABLED) 1F else 0.5F
     }
 
     private fun setButtonSizeLarge() {
-        setTextAppearance(R.style.TextButtonLarge)
+        textTV.setTextAppearance(R.style.TextButtonLarge)
         layoutParams.apply {
             setPadding(
                 resources.getDimensionPixelSize(R.dimen.button_padding_20),
@@ -86,62 +107,45 @@ class InnoProgButtonView @JvmOverloads constructor(
                 resources.getDimensionPixelSize(R.dimen.button_padding_12),
             )
         }
-}
+        setIconLayoutParam()
+    }
 
     private fun setButtonSizeMedium() {
-        setTextAppearance(R.style.TextButtonMedium)
-        height = context.resources.getDimension(R.dimen.button_height_40).toInt()
+        textTV.setTextAppearance(R.style.TextButtonMedium)
         layoutParams.apply {
-            gravity = Gravity.CENTER
             setPadding(
-                context.resources.getDimension(R.dimen.button_padding_18).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_10).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_18).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_10).toInt(),
+                resources.getDimensionPixelSize(R.dimen.button_padding_18),
+                resources.getDimensionPixelSize(R.dimen.button_padding_10),
+                resources.getDimensionPixelSize(R.dimen.button_padding_18),
+                resources.getDimensionPixelSize(R.dimen.button_padding_10),
             )
         }
+        setIconLayoutParam()
     }
 
     private fun setButtonSizeSmall() {
-        setTextAppearance(R.style.TextButtonSmall)
-        height = context.resources.getDimension(R.dimen.button_height_32).toInt()
-        layoutParams.apply {
-            gravity = Gravity.CENTER
-            setPadding(
-                context.resources.getDimension(R.dimen.button_padding_12).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_6).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_12).toInt(),
-                context.resources.getDimension(R.dimen.button_padding_6).toInt(),
-            )
-        }
+        textTV.setTextAppearance(R.style.TextButtonSmall)
+        setPadding(
+            resources.getDimensionPixelSize(R.dimen.button_padding_12),
+            resources.getDimensionPixelSize(R.dimen.button_padding_6),
+            resources.getDimensionPixelSize(R.dimen.button_padding_12),
+            resources.getDimensionPixelSize(R.dimen.button_padding_6),
+        )
+        setIconLayoutParam()
     }
 
     private fun setIcons() {
-
-        leftIcon?.let {
-            it.setTint(currentHintTextColor)
-            it.setBounds(
-                0,
-                0,
-                selectIconSize(),
-                selectIconSize()
-            )
+        leftIconDrawable?.let {
+            it.setTint(textTV.currentTextColor)
+            leftIconIV.setImageDrawable(it)
         }
-        rightIcon?.let {
-            it.setTint(currentHintTextColor)
-            it.setBounds(
-                0,
-                0,
-                selectIconSize(),
-                selectIconSize()
-            )
+        rightIconDrawable?.let {
+            it.setTint(textTV.currentTextColor)
+            rightIconIV.setImageDrawable(it)
         }
-        setCompoundDrawables(leftIcon, null, rightIcon, null)
-        compoundDrawablePadding = selectIconPadding()
-
     }
 
-    private fun selectIconPadding(): Int {
+    private fun selectIconMargin(): Int {
         return when (size) {
             ButtonSize.LARGE -> resources.getDimensionPixelSize(R.dimen.button_padding_14)
             ButtonSize.MEDIUM -> resources.getDimensionPixelSize(R.dimen.button_padding_14)
@@ -150,23 +154,28 @@ class InnoProgButtonView @JvmOverloads constructor(
     }
 
     private fun selectIconSize(): Int {
-        return when (size) {
-            ButtonSize.SMALL -> resources.getDimensionPixelSize(R.dimen.button_icon_size_16)
-            else -> resources.getDimensionPixelSize(R.dimen.button_icon_size_20)
+        return if (size == ButtonSize.SMALL) resources.getDimensionPixelSize(R.dimen.button_icon_size_16)
+        else resources.getDimensionPixelSize(R.dimen.button_icon_size_20)
+    }
+
+    private fun setIconLayoutParam() {
+        leftIconDrawable.let {
+            val marginParam = textTV.layoutParams as MarginLayoutParams
+            marginParam.marginStart = selectIconMargin()
+            textTV.layoutParams = marginParam
+            leftIconIV.layoutParams.apply {
+                height = selectIconSize()
+                width = selectIconSize()
+            }
         }
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        setType()
-        setIcons()
-    }
-
-    enum class ButtonType {
-        PRIMARY, DEFAULT, FLAT
-    }
-
-    enum class ButtonSize {
-        LARGE, MEDIUM, SMALL
+        rightIconDrawable.let {
+            val marginParam = textTV.layoutParams as MarginLayoutParams
+            marginParam.marginEnd = selectIconMargin()
+            textTV.layoutParams = marginParam
+            rightIconIV.layoutParams.apply {
+                height = selectIconSize()
+                width = selectIconSize()
+            }
+        }
     }
 }
