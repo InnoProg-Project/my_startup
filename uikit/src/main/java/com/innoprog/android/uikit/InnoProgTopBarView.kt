1e6c5d27c5b0c541d7993528ce100387cdc84ff0
Andrey_W <Uzzbagoyzza@yandex.ru>
2024-03-20 17:17:28 +0300

    Создал класс, лайаут и параметры к нему.

---
+++ uikit/src/main/java/com/innoprog/android/uikit/InnoProgTopBarView.kt
@@ -0,0 +1,12 @@
+package com.innoprog.android.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class InnoProgTopBarView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+}
