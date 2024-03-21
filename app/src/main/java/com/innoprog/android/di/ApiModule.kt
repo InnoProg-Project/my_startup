89af59f3d551b3ed810defb0b1d34cb94dde0132
NadezhdaSakal <sakaln88@gmail.com>
2024-03-21 22:34:27 +0300

    fix ProfileModuleComponent

--- app/src/main/java/com/innoprog/android/di/ApiModule.kt
+++ app/src/main/java/com/innoprog/android/di/ApiModule.kt
@@ -1,16 +1,13 @@
 package com.innoprog.android.di
 
-import com.innoprog.android.data.network.ApiService
 import com.innoprog.android.domain.ApiInteractor
 import com.innoprog.android.domain.ApiInteractorImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 @Module
-class ApiModule {
+interface ApiModule {
 
-    @Provides
-    fun provideApiInteractor(apiService: ApiService): ApiInteractor {
-        return ApiInteractorImpl(apiService)
-    }
+    @Binds
+    fun provideApiInteractor(impl: ApiInteractorImpl): ApiInteractor
 }
--- app/src/main/java/com/innoprog/android/feature/profile/di/ProfileModule.kt
+++ app/src/main/java/com/innoprog/android/feature/profile/di/ProfileModule.kt
@@ -2,8 +2,6 @@ package com.innoprog.android.feature.profile.di
 
 import androidx.lifecycle.ViewModel
 import com.innoprog.android.di.ViewModelKey
-import com.innoprog.android.domain.ApiInteractor
-import com.innoprog.android.domain.ApiInteractorImpl
 import com.innoprog.android.feature.profile.presentation.ProfileViewModel
 import dagger.Binds
 import dagger.Module
@@ -16,7 +14,4 @@ interface ProfileModule {
     @ViewModelKey(ProfileViewModel::class)
     @Binds
     fun bindProfileViewModel(impl: ProfileViewModel): ViewModel
-
-    @Binds
-    fun bindApiInteractor(impl: ApiInteractorImpl): ApiInteractor
 }
