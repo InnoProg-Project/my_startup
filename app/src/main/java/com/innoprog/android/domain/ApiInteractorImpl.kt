802ec9c30d074de9b2d23abcdee4cc9a5c02da24
NadezhdaSakal <sakaln88@gmail.com>
2024-03-21 21:19:49 +0300

    fix after detekt

--- app/src/main/java/com/innoprog/android/domain/ApiInteractorImpl.kt
+++ app/src/main/java/com/innoprog/android/domain/ApiInteractorImpl.kt
@@ -1,27 +1,22 @@
 package com.innoprog.android.domain
 
 import com.innoprog.android.data.network.ApiService
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import javax.inject.Inject
 
 class ApiInteractorImpl @Inject constructor(
-    apiMethodsProvider: ApiService,
+    private val apiService: ApiService
 ) : ApiInteractor {
 
-    private val apiMethods = apiMethodsProvider.provide()
-
     override fun post(url: String, body: Any, headers: Map<String, String>): Call<ResponseBody> {
-        val response = when (body) {
-            is String -> apiMethods.postRaw(url = url, body = body, headers = headers)
-            else -> apiMethods.post(url = url, body = body, headers = headers)
-        }.execute()
-
-        return response
+        val requestBody = RequestBody.create("text/plain".toMediaType(), body.toString())
+        return apiService.post(url, requestBody, headers)
     }
 
     override fun get(url: String, headers: Map<String, String>): Call<ResponseBody> {
-        val response = apiMethods.get(url = url, headers = headers).execute()
-        return response
+        return apiService.get(url, headers)
     }
 }
