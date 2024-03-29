08653af37e5011b407cf0497338c7360e752f698
TanyaRul <t.rul26@yandex.ru>
2024-03-29 11:18:42 +0300

    - changes the FeedFragment layout - adds a publication layout

--- app/src/main/java/com/innoprog/android/feature/feed/presentation/FeedFragment.kt
+++ app/src/main/java/com/innoprog/android/feature/feed/presentation/FeedFragment.kt
@@ -1,12 +1,15 @@
 package com.innoprog.android.feature.feed.presentation
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.innoprog.android.base.BaseFragment
 import com.innoprog.android.base.BaseViewModel
 import com.innoprog.android.databinding.FragmentFeedBinding
 import com.innoprog.android.di.ScreenComponent
 import com.innoprog.android.feature.feed.di.DaggerFeedComponent
+import com.innoprog.android.uikit.InnoProgChipGroupView
 
 class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
 
@@ -18,5 +21,28 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
         container: ViewGroup?
     ): FragmentFeedBinding {
         return FragmentFeedBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initChips()
+    }
+
+    private fun initChips() {
+        val chipTitles = listOf(ALL_CONTENT, PROJECT, IDEAS)
+        binding.cgvFilter.setChips(chipTitles)
+        binding.cgvFilter.setOnChipSelectListener(object :
+            InnoProgChipGroupView.OnChipSelectListener {
+            override fun onChipSelected(chipIndex: Int) {
+                // Если нужно обработать чип
+            }
+        })
+    }
+
+    companion object {
+        private const val ALL_CONTENT = "Всё"
+        private const val PROJECT = "Проекты"
+        private const val IDEAS = "Идеи"
     }
 }
