67961b9625d2595567cec4609fce861e24ffc2c8
Anta <ovaorange@yandex.ru>
2024-04-08 11:57:12 +0200

    only layout

--- app/src/main/java/com/innoprog/android/feature/auth/passwordrecovery/presentation/PasswordRecoveryFragment.kt
+++ app/src/main/java/com/innoprog/android/feature/auth/passwordrecovery/presentation/PasswordRecoveryFragment.kt
@@ -25,7 +25,7 @@ class PasswordRecoveryFragment : BaseFragment<FragmentPasswordRecoveryBinding, B
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnBack.setOnClickListener {
+        binding.topBar.setLeftIconClickListener {
             viewModel.navigateUp()
         }
 
