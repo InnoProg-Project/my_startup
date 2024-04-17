7b174cdfee4791e6f055f12871b452e5cbf141c3
NadezhdaSakal <sakaln88@gmail.com>
2024-04-17 18:42:47 +0300

    fix after detekt

--- app/src/main/java/com/innoprog/android/feature/profile/profiledetails/presentation/ProfileFragment.kt
+++ app/src/main/java/com/innoprog/android/feature/profile/profiledetails/presentation/ProfileFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.innoprog.android.R
 import com.innoprog.android.base.BaseFragment
 import com.innoprog.android.base.BaseViewModel
