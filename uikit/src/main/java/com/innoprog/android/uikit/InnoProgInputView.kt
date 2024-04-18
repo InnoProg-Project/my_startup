cfd7680fc34e4b3602491426b1e00ebc9cbd3a56
Andrey_W <Uzzbagoyzza@yandex.ru>
2024-04-18 18:11:55 +0300

    попрвил функцию по установке текста для InputText

--- uikit/src/main/java/com/innoprog/android/uikit/InnoProgInputView.kt
+++ uikit/src/main/java/com/innoprog/android/uikit/InnoProgInputView.kt
@@ -235,7 +235,10 @@ class InnoProgInputView @JvmOverloads constructor(
     }
 
     fun setText(text: String) {
-        editTextView.setText(text)
+        if (text.isNotBlank()) {
+            showHintFilled()
+            editTextView.setText(text)
+        }
     }
 
     companion object {
