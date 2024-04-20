7623b2d6e99d7f9cfafe5060215a4f2af73cb4b4
NadezhdaSakal <sakaln88@gmail.com>
2024-04-20 21:00:11 +0300

    add EditingDocksFragment

---
+++ app/src/main/java/com/innoprog/android/feature/projects/presentation/editingdocks/EditingDocksViewModel.kt
@@ -0,0 +1,6 @@
+package com.innoprog.android.feature.projects.presentation.editingdocks
+
+import com.innoprog.android.base.BaseViewModel
+import javax.inject.Inject
+
+class EditingDocksViewModel @Inject constructor() : BaseViewModel()
