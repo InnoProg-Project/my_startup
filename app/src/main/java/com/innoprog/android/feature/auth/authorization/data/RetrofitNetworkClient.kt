4ac18a2a2fb0cca5724775290c284f6b5d3a917a
Anta <ovaorange@yandex.ru>
2024-05-01 10:49:07 +0200

    fix after detect

--- app/src/main/java/com/innoprog/android/feature/auth/authorization/data/RetrofitNetworkClient.kt
+++ app/src/main/java/com/innoprog/android/feature/auth/authorization/data/RetrofitNetworkClient.kt
@@ -4,7 +4,6 @@ import com.innoprog.android.network.data.ApiService
 import com.innoprog.android.network.data.Response
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.io.IOException
 import javax.inject.Inject
 
 class RetrofitNetworkClient @Inject constructor(
@@ -16,7 +15,7 @@ class RetrofitNetworkClient @Inject constructor(
             try {
                 val response = api.authorize(dto)
                 response.apply { resultCode = SUCCESS }
-            } catch (e: IOException) {
+            } catch (e: RuntimeException) {
                 Response().apply { resultCode = ERROR_CODE }
             }
         }
