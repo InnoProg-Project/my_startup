a3645a061753308f523357064c0944401b1b69f0
NadezhdaSakal <sakaln88@gmail.com>
2024-05-08 13:05:07 +0300

    add SerializedName annotation

--- app/src/main/java/com/innoprog/android/feature/profile/profiledetails/data/network/ProfileResponse.kt
+++ app/src/main/java/com/innoprog/android/feature/profile/profiledetails/data/network/ProfileResponse.kt
@@ -1,12 +1,18 @@
 package com.innoprog.android.feature.profile.profiledetails.data.network
 
+import com.google.gson.annotations.SerializedName
 import com.innoprog.android.feature.profile.profiledetails.domain.models.CommunicationChannel
 import com.innoprog.android.network.data.Response
 
 data class ProfileResponse(
+    @SerializedName("userId")
     val userId: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("about")
     val about: String,
+    @SerializedName("communicationChannels")
     val communicationChannels: List<CommunicationChannel>,
+    @SerializedName("authorities")
     val authorities: List<String>
 ) : Response()
