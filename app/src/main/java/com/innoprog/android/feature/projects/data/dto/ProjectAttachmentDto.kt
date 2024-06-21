eaf2eee11c64d757bef575628b495ddcdd0971d4
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-22 02:32:33 +0300

    180_добавляет модели данных

---
+++ app/src/main/java/com/innoprog/android/feature/projects/data/dto/ProjectAttachmentDto.kt
@@ -0,0 +1,10 @@
+package com.innoprog.android.feature.projects.data.dto
+
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class ProjectAttachmentDto(
+    @SerializedName("id") val id: UUID,
+    @SerializedName("filePath") val filePath: String,
+    @SerializedName("type") val type: String
+)
---
+++ app/src/main/java/com/innoprog/android/feature/projects/domain/models/Project.kt
@@ -0,0 +1,19 @@
+package com.innoprog.android.feature.projects.domain.models
+
+import java.util.Date
+import java.util.UUID
+
+data class Project(
+    val id: UUID,
+    val name: String,
+    val shortDescription: String,
+    val description: String,
+    val logoFilePath: String,
+    val publicationsCount: Int,
+    val area: String,
+    val financingStage: String,
+    val deadline: Date,
+    val siteUrls: String,
+    val documentUrls: List<String>,
+    val projectAttachments: List<ProjectAttachment>
+)
