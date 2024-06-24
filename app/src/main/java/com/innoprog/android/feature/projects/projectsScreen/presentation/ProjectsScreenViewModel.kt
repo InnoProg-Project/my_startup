13a165f63868fe80e4ebe2732d1208ffe454fcef
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-24 14:25:17 +0300

    180_расширяет ProjectsScreenState и провайдит stateflow во viewmodel

--- app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/presentation/ProjectsScreenViewModel.kt
+++ app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/presentation/ProjectsScreenViewModel.kt
@@ -1,29 +1,49 @@
 package com.innoprog.android.feature.projects.projectsScreen.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.innoprog.android.base.BaseViewModel
-import com.innoprog.android.feature.projects.projectsScreen.domain.useCase.GetProjectsUseCase
+import com.innoprog.android.feature.projects.projectsScreen.domain.api.GetProjectListUseCase
+import com.innoprog.android.util.ErrorScreenState
+import com.innoprog.android.util.ErrorType
+import com.innoprog.android.util.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ProjectsScreenViewModel @Inject constructor(
-    private val getProjectsUseCase: GetProjectsUseCase,
+    private val getProjectListUseCase: GetProjectListUseCase
 ) : BaseViewModel() {
 
-    private val _state = MutableLiveData<ProjectsScreenState>()
-    val state: LiveData<ProjectsScreenState> get() = _state
+    private val _state = MutableStateFlow<ProjectsScreenState>(ProjectsScreenState.Loading)
+    val state = _state.asStateFlow()
 
     init {
         viewModelScope.launch {
-            getProjectsUseCase.execute().collect {
-                if (it.first != null) {
-                    _state.value = ProjectsScreenState.Content(it.first!!)
-                } else {
-                    _state.value = ProjectsScreenState.Empty
+            renderState()
+        }
+    }
+
+
+    private suspend fun renderState() {
+        getProjectListUseCase.execute("01", null, 10).collect { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    _state.value = ProjectsScreenState.Content(resource.data)
+                }
+
+                is Resource.Error -> {
+                    _state.value = renderError(resource as ErrorType)
                 }
             }
+        }
+    }
+
+    private fun renderError(errorType: ErrorType) : ProjectsScreenState.Error {
+        return when(errorType) {
+            ErrorType.NO_CONNECTION -> ProjectsScreenState.Error(ErrorScreenState.NO_INTERNET)
+            ErrorType.NOT_FOUND -> ProjectsScreenState.Error(ErrorScreenState.NOT_FOUND)
+            else -> ProjectsScreenState.Error(ErrorScreenState.SERVER_ERROR)
         }
     }
 }
