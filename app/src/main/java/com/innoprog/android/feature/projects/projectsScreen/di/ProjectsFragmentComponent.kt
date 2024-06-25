2c4a9c00be9093799d4695264853e6a0c569f0cb
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-25 22:53:29 +0300

    180_првоайдит зависимости

--- app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/di/ProjectsFragmentComponent.kt
+++ app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/di/ProjectsFragmentComponent.kt
@@ -1,18 +1,11 @@
 import com.innoprog.android.di.AppComponent
 import com.innoprog.android.di.ScreenComponent
-import com.innoprog.android.feature.projects.projectsScreen.di.ProjectListDataModule
-import com.innoprog.android.feature.projects.projectsScreen.di.ProjectListUseCaseModule
 import com.innoprog.android.feature.projects.projectsScreen.di.ProjectsFragmentModule
-import com.innoprog.android.network.data.NetworkModule
 import dagger.Component
 
 @Component(
     dependencies = [AppComponent::class],
-    modules = [
-        ProjectsFragmentModule::class,
-        ProjectListDataModule::class,
-        ProjectListUseCaseModule::class
-    ]
+    modules = [ProjectsFragmentModule::class]
 )
 interface ProjectsFragmentComponent : ScreenComponent {
     @Component.Builder
--- app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/di/ProjectsFragmentModule.kt
+++ app/src/main/java/com/innoprog/android/feature/projects/projectsScreen/di/ProjectsFragmentModule.kt
@@ -2,16 +2,42 @@ package com.innoprog.android.feature.projects.projectsScreen.di
 
 import androidx.lifecycle.ViewModel
 import com.innoprog.android.di.ViewModelKey
+import com.innoprog.android.feature.projects.domain.api.ProjectRepository
+import com.innoprog.android.feature.projects.projectsScreen.data.network.ProjectApiService
+import com.innoprog.android.feature.projects.projectsScreen.data.network.ProjectListNetworkClient
+import com.innoprog.android.feature.projects.projectsScreen.data.network.ProjectListNetworkClientImpl
+import com.innoprog.android.feature.projects.projectsScreen.data.repository.ProjectRepositoryImpl
+import com.innoprog.android.feature.projects.projectsScreen.domain.api.GetProjectListUseCase
+import com.innoprog.android.feature.projects.projectsScreen.domain.impl.GetProjectListUseCaseImpl
 import com.innoprog.android.feature.projects.projectsScreen.presentation.ProjectsScreenViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.multibindings.IntoMap
+import retrofit2.Retrofit
 
-@Module
+@Module(includes = [ProjectsFragmentModule.ProjectApiModule::class])
 interface ProjectsFragmentModule {
 
     @IntoMap
     @ViewModelKey(ProjectsScreenViewModel::class)
     @Binds
     fun bindVM(impl: ProjectsScreenViewModel): ViewModel
+
+    @Binds
+    fun bindNetworkCliend(impl: ProjectListNetworkClientImpl) : ProjectListNetworkClient
+
+    @Binds
+    fun bundRepository(impl: ProjectRepositoryImpl) : ProjectRepository
+
+    @Binds
+    fun bindProjectListUseCase(impl: GetProjectListUseCaseImpl) : GetProjectListUseCase
+
+    @Module
+    class ProjectApiModule {
+        @Provides
+        fun provideApi(retrofit: Retrofit): ProjectApiService {
+            return retrofit.create(ProjectApiService::class.java)
+        }
+    }
 }
