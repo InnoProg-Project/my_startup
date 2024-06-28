ef031ecff0903fad992b612b8f94848389df9777
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-06-28 14:22:08 +0300

    52_корректирует di переопределяет макет удаляет неиспользуемый макет

--- app/src/main/java/com/innoprog/android/feature/feed/userprojectscreen/di/UserProjectComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.innoprog.android.feature.feed.userprojectscreen.di
-
-import com.innoprog.android.di.ScreenComponent
-import dagger.Component
-
-@Component(
-    modules = [UserProjectModule::class]
-)
-interface UserProjectComponent : ScreenComponent
--- app/src/main/java/com/innoprog/android/feature/feed/userprojectscreen/presentation/UserProjectDetailsFragment.kt
+++ app/src/main/java/com/innoprog/android/feature/feed/userprojectscreen/presentation/UserProjectDetailsFragment.kt
@@ -1,24 +1,46 @@
 package com.innoprog.android.feature.feed.userprojectscreen.presentation
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.innoprog.android.base.BaseFragment
-import com.innoprog.android.databinding.FragmentUserProjectDetailsBinding
-import com.innoprog.android.di.ScreenComponent
-import com.innoprog.android.feature.feed.userprojectscreen.di.DaggerUserProjectComponent
+import com.innoprog.android.databinding.FragmentAnyProjectBinding
+import com.innoprog.android.di.AppComponentHolder
+import com.innoprog.android.feature.feed.anyProjectDetails.presentation.DocumentAdapter
+import com.innoprog.android.feature.feed.userprojectscreen.di.DaggerUserProjectDetailsComponent
+import com.innoprog.android.feature.training.common.VerticalSpaceDecorator
+import com.innoprog.android.uikit.R
 
 class UserProjectDetailsFragment :
-    BaseFragment<FragmentUserProjectDetailsBinding, UserProjectViewModel>() {
+    BaseFragment<FragmentAnyProjectBinding, UserProjectViewModel>() {
 
     override val viewModel by injectViewModel<UserProjectViewModel>()
-    override fun diComponent(): ScreenComponent = DaggerUserProjectComponent.builder().build()
+    val projectId: String? by lazy {
+        arguments?.getString(PROJECT_ID)
+    }
+
+    override fun diComponent() = DaggerUserProjectDetailsComponent
+        .builder()
+        .appComponent(AppComponentHolder.getComponent())
+        .build()
+
+    private val documentAdapter = DocumentAdapter { uri ->
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(uri)))
+    }
+
+    private val decorator: VerticalSpaceDecorator by lazy {
+        VerticalSpaceDecorator(resources.getDimensionPixelSize(R.dimen.margin_16))
+    }
 
     override fun createBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentUserProjectDetailsBinding {
-        return FragmentUserProjectDetailsBinding.inflate(inflater, container, false)
+    ): FragmentAnyProjectBinding {
+        return FragmentAnyProjectBinding.inflate(inflater, container, false)
     }
 
-
+    companion object {
+        const val PROJECT_ID = "project_id"
+    }
 }
