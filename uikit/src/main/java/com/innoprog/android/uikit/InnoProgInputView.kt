3cd4e99ed89da2d63c632827247ada1c4c00a82f
Zedd3d <yariyan@yandex.ru>
2024-07-02 18:06:35 +0300

    - сильно переделал кастом вью InnoProgInputView, убрал EditText, добавил TextInputLayout и TextInputEditText. Оставил пока без изменений картинки, но в теории их тоже можно убрать, просто это дополнительное время, пока работает и так (там нужно тогда переделывать логику и реализацию "глаза" в пароле и может что-то ещё)

--- uikit/src/main/java/com/innoprog/android/uikit/InnoProgInputView.kt
+++ uikit/src/main/java/com/innoprog/android/uikit/InnoProgInputView.kt
@@ -12,14 +12,14 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat.getDrawable
-import androidx.transition.TransitionManager
+import androidx.core.view.isVisible
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.innoprog.android.uikit.ext.applyStyleable
 
 class InnoProgInputView @JvmOverloads constructor(
@@ -33,9 +33,9 @@ class InnoProgInputView @JvmOverloads constructor(
 ) {
     private var state: InnoProgInputViewState = InnoProgInputViewState.INACTIVE
 
-    private val editTextView by lazy { findViewById<EditText>(R.id.edit_text) }
+    private val tilEdit by lazy { findViewById<TextInputLayout>(R.id.til_edit) }
+    private val editTextView by lazy { findViewById<TextInputEditText>(R.id.edit_text) }
     private val captionTextView by lazy { findViewById<TextView>(R.id.caption) }
-    private val emptyHintTextView by lazy { findViewById<TextView>(R.id.hint_empty) }
     private val leftIcon by lazy { findViewById<ImageView>(R.id.left_icon) }
     private val rightIcon by lazy { findViewById<ImageView>(R.id.right_icon) }
     private val backgroundEditTextView by lazy { findViewById<ConstraintLayout>(R.id.background_edit_text_view) }
@@ -45,12 +45,6 @@ class InnoProgInputView @JvmOverloads constructor(
             context,
             R.drawable.inno_prog_input_view_background
         ) as LayerDrawable
-    }
-
-    private val constraintSet by lazy {
-        ConstraintSet().apply {
-            clone(backgroundEditTextView)
-        }
     }
 
     private val focusChangeListener by lazy {
@@ -103,9 +97,13 @@ class InnoProgInputView @JvmOverloads constructor(
 
             captionTextView.text = getString(R.styleable.InnoProgInputView_caption)
             leftIcon.setImageDrawable(getDrawable(R.styleable.InnoProgInputView_left_icon))
+            leftIcon.isVisible = leftIcon.drawable != null
             rightIcon.setImageDrawable(getDrawable(R.styleable.InnoProgInputView_right_icon))
-            emptyHintTextView.text = getString(R.styleable.InnoProgInputView_label)
+
             getString(R.styleable.InnoProgInputView_text)?.let { setText(it) }
+            editTextView.minHeight =
+                getDimensionPixelSize(R.styleable.InnoProgInputView_minHeightEditText, 0)
+            tilEdit.hint = getString(R.styleable.InnoProgInputView_label)
         }
 
         isFocusable = true
@@ -114,15 +112,7 @@ class InnoProgInputView @JvmOverloads constructor(
         this@InnoProgInputView.onFocusChangeListener = focusChangeListener
 
         editTextView.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                showHintFilled()
-                renderState(InnoProgInputViewState.FOCUSED)
-            } else {
-                if (editTextView.text.isNullOrEmpty()) {
-                    showHintEmpty()
-                }
-                renderState(state)
-            }
+            renderState(state)
         }
     }
 
@@ -140,6 +130,7 @@ class InnoProgInputView @JvmOverloads constructor(
 
     fun setRightIcon(src: Int) {
         rightIcon.setImageDrawable(getDrawable(context, src))
+        rightIcon.isVisible = rightIcon.drawable != null
     }
 
     override fun onSaveInstanceState(): Parcelable {
@@ -198,47 +189,6 @@ class InnoProgInputView @JvmOverloads constructor(
         inputManager.showSoftInput(editTextView, 0)
     }
 
-    private fun showHintEmpty() {
-        emptyHintTextView.textSize = SP_16
-
-        constraintSet.connect(
-            R.id.hint_empty,
-            ConstraintSet.TOP,
-            R.id.left_icon,
-            ConstraintSet.TOP
-        )
-        constraintSet.connect(
-            R.id.hint_empty,
-            ConstraintSet.BOTTOM,
-            R.id.left_icon,
-            ConstraintSet.BOTTOM
-        )
-
-        TransitionManager.beginDelayedTransition(backgroundEditTextView)
-        constraintSet.applyTo(backgroundEditTextView)
-    }
-
-    private fun showHintFilled() {
-        emptyHintTextView.textSize = SP_12
-
-        constraintSet.connect(
-            R.id.hint_empty,
-            ConstraintSet.TOP,
-            ConstraintSet.PARENT_ID,
-            ConstraintSet.TOP
-        )
-        constraintSet.clear(R.id.hint_empty, ConstraintSet.BOTTOM)
-
-        constraintSet.setMargin(
-            R.id.hint_empty,
-            ConstraintSet.TOP,
-            resources.getDimensionPixelSize(R.dimen.margin_5)
-        )
-
-        TransitionManager.beginDelayedTransition(backgroundEditTextView)
-        constraintSet.applyTo(backgroundEditTextView)
-    }
-
     fun setLeftIconClickListener(onClickListener: OnClickListener) {
         leftIcon.setOnClickListener(onClickListener)
     }
@@ -255,13 +205,8 @@ class InnoProgInputView @JvmOverloads constructor(
         return editTextView.text.toString()
     }
 
-    fun setHintText(text: String) {
-        emptyHintTextView.text = text
-    }
-
     fun setText(text: String) {
         if (text.isNotBlank()) {
-            showHintFilled()
             editTextView.setText(text)
         }
     }
@@ -277,11 +222,6 @@ class InnoProgInputView @JvmOverloads constructor(
     private companion object {
         const val FULL_VISIBLE = 1f
         const val VISIBILITY_40_PERCENT = 0.4f
-
-        const val SP_12 = 12f
-        const val SP_16 = 16f
-
-        const val KEY_SUPER_STATE = "superState"
     }
 }
 
