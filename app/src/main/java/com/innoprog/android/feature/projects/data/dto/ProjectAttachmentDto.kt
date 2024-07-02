f7ffa97cc0d7c2ff1cc3dea07bc600644b952079
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-02 16:24:30 +0300

    180_убирает сериализацию из моделей

--- app/src/main/java/com/innoprog/android/feature/projects/data/dto/ProjectAttachmentDto.kt
+++ app/src/main/java/com/innoprog/android/feature/projects/data/dto/ProjectAttachmentDto.kt
@@ -1,10 +1,9 @@
 package com.innoprog.android.feature.projects.data.dto
 
-import com.google.gson.annotations.SerializedName
 import java.util.UUID
 
 data class ProjectAttachmentDto(
-    @SerializedName("id") val id: UUID,
-    @SerializedName("filePath") val filePath: String,
-    @SerializedName("type") val type: String
+    val id: UUID,
+    val filePath: String,
+    val type: String
 )
--- app/src/main/java/com/innoprog/android/feature/projects/data/dto/ProjectDto.kt
+++ app/src/main/java/com/innoprog/android/feature/projects/data/dto/ProjectDto.kt
@@ -1,20 +1,19 @@
 package com.innoprog.android.feature.projects.data.dto
 
-import com.google.gson.annotations.SerializedName
 import java.util.Date
 import java.util.UUID
 
 data class ProjectDto(
-    @SerializedName("id") val id: UUID,
-    @SerializedName("name") val name: String,
-    @SerializedName("shortDescription") val shortDescription: String,
-    @SerializedName("description") val description: String,
-    @SerializedName("logoFilePath") val logoFilePath: String,
-    @SerializedName("publicationsCount") val publicationsCount: Int,
-    @SerializedName("area") val area: String,
-    @SerializedName("financingStage") val financingStage: String,
-    @SerializedName("deadline") val deadline: Date,
-    @SerializedName("siteUrls") val siteUrls: String,
-    @SerializedName("documentUrls") val documentUrls: List<String>,
-    @SerializedName("projectAttachments") val projectAttachments: List<ProjectAttachmentDto>
+    val id: UUID,
+    val name: String,
+    val shortDescription: String,
+    val description: String,
+    val logoFilePath: String,
+    val publicationsCount: Int,
+    val area: String,
+    val financingStage: String,
+    val deadline: Date,
+    val siteUrls: String,
+    val documentUrls: List<String>,
+    val projectAttachments: List<ProjectAttachmentDto>
 )
