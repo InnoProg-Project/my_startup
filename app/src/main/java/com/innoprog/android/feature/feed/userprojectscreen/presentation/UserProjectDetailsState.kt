afec15a7c00a2cb5efa9e236fb23aa4f0a848d21
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-04 16:48:22 +0300

    52_корректирует классы стэйтов

--- app/src/main/java/com/innoprog/android/feature/feed/userprojectscreen/presentation/UserProjectDetailsState.kt
+++ app/src/main/java/com/innoprog/android/feature/feed/userprojectscreen/presentation/UserProjectDetailsState.kt
@@ -6,6 +6,6 @@ import com.innoprog.android.util.ErrorScreenState
 sealed class UserProjectDetailsState {
     data object Empty : UserProjectDetailsState()
     data object Loading : UserProjectDetailsState()
-    data class Error(val errorType: ErrorScreenState) : UserProjectDetailsState()
-    data class Content(val project: AnyProjectDetailsModel) : UserProjectDetailsState()
+    class Error(val errorType: ErrorScreenState) : UserProjectDetailsState()
+    class Content(val project: AnyProjectDetailsModel) : UserProjectDetailsState()
 }
