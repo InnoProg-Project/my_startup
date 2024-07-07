56f1714dcea8a220d1c92383565aaaec3c7c4d4e
NadezhdaSakal <sakaln88@gmail.com>
2024-07-07 22:44:06 +0300

    refactor sealed interfaces

--- app/src/main/java/com/innoprog/android/feature/profile/profiledetails/data/network/RequestByProfile.kt
+++ app/src/main/java/com/innoprog/android/feature/profile/profiledetails/data/network/RequestByProfile.kt
@@ -3,9 +3,9 @@ package com.innoprog.android.feature.profile.profiledetails.data.network
 sealed interface RequestByProfile {
     data object GetProfile : RequestByProfile
     data object GetProfileCompany : RequestByProfile
-    data class GetAll(val authorId: String) : RequestByProfile
-    data class GetProjects(val type: String, val authorId: String) : RequestByProfile
-    data class GetIdeas(val type: String, val authorId: String) : RequestByProfile
-    data class GetLikes(val pageSize: Int) : RequestByProfile
-    data class GetFavorites(val pageSize: Int) : RequestByProfile
+    class GetAll(val authorId: String) : RequestByProfile
+    class GetProjects(val type: String, val authorId: String) : RequestByProfile
+    class GetIdeas(val type: String, val authorId: String) : RequestByProfile
+    class GetLikes(val pageSize: Int) : RequestByProfile
+    class GetFavorites(val pageSize: Int) : RequestByProfile
 }
--- app/src/main/java/com/innoprog/android/feature/profile/profiledetails/presentation/ChipsScreenState.kt
+++ app/src/main/java/com/innoprog/android/feature/profile/profiledetails/presentation/ChipsScreenState.kt
@@ -4,15 +4,15 @@ import com.innoprog.android.feature.profile.profiledetails.domain.models.FeedWra
 import com.innoprog.android.util.ErrorType
 
 sealed interface ChipsScreenState {
-    data class All(val content: List<FeedWrapper>) : ChipsScreenState
+    class All(val content: List<FeedWrapper>) : ChipsScreenState
 
-    data class Projects(val projects: List<FeedWrapper.News>) : ChipsScreenState
+    class Projects(val projects: List<FeedWrapper.News>) : ChipsScreenState
 
-    data class Ideas(val ideas: List<FeedWrapper.Idea>) : ChipsScreenState
+    class Ideas(val ideas: List<FeedWrapper.Idea>) : ChipsScreenState
 
-    data class Liked(val liked: List<FeedWrapper>) : ChipsScreenState
+    class Liked(val liked: List<FeedWrapper>) : ChipsScreenState
 
-    data class Favorites(val favorites: List<FeedWrapper>) : ChipsScreenState
+    class Favorites(val favorites: List<FeedWrapper>) : ChipsScreenState
 
-    data class Error(val type: ErrorType) : ChipsScreenState
+    class Error(val type: ErrorType) : ChipsScreenState
 }
