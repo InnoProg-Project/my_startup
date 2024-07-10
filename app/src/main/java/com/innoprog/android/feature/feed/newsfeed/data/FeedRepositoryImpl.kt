7a686b93b85135a3e1d76a66aeb9dfead7cac4b8
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-11 01:15:44 +0300

    201_подключает провайд обработки ошибок в репо

--- app/src/main/java/com/innoprog/android/feature/feed/newsfeed/data/FeedRepositoryImpl.kt
+++ app/src/main/java/com/innoprog/android/feature/feed/newsfeed/data/FeedRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.innoprog.android.feature.feed.newsfeed.data
 
 import android.util.Log
+import com.google.gson.JsonParseException
 import com.innoprog.android.BuildConfig
 import com.innoprog.android.feature.feed.newsfeed.data.converters.mapToNewsDomain
 import com.innoprog.android.feature.feed.newsfeed.data.converters.mapToProjectDomain
@@ -13,33 +14,50 @@ import com.innoprog.android.feature.feed.newsfeed.domain.models.NewsWithProject
 import com.innoprog.android.feature.feed.newsfeed.domain.models.Project
 import com.innoprog.android.feature.feed.newsfeed.domain.models.QueryPage
 import com.innoprog.android.network.data.ApiConstants
+import com.innoprog.android.util.ErrorHandler
+import com.innoprog.android.util.ErrorHandlerImpl
 import com.innoprog.android.util.ErrorType
 import com.innoprog.android.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 class FeedRepositoryImpl @Inject constructor(private val networkClient: NetworkClient) :
     FeedRepository {
+    private val errorHandler: ErrorHandler = ErrorHandlerImpl()
 
     override fun getNewsFeed(queryPage: QueryPage): Flow<Resource<List<NewsWithProject>>> = flow {
-        val newsResult = loadNewsList(queryPage)
-
-        if (newsResult is Resource.Error) {
-            emit(newsResult)
-        } else if (newsResult is Resource.Success) {
-            val newsList = newsResult.data
-
-            val newsWithProjects = newsList.map { news ->
-                val project = if (news.projectId != null) {
-                    loadProjectDetails(news.projectId)
-                } else {
-                    null
+        try {
+            val response = networkClient.loadNewsFeed(queryPage)
+            when (response.resultCode) {
+                ApiConstants.SUCCESS_CODE -> {
+                    val newsList = (response as? FeedResponse)?.results ?: emptyList()
+                    val newsWithProjects = newsList.map { news ->
+                        val project = loadProjectDetails(news.projectId)
+                        NewsWithProject(news.mapToNewsDomain(), project)
+                    }
+                    emit(Resource.Success(newsWithProjects))
                 }
-                NewsWithProject(news, project)
+                else -> emit(errorHandler.handleErrorCode(response.resultCode))
             }
-            emit(Resource.Success(newsWithProjects))
+        } catch (e: HttpException) {
+            Log.e(TAG, "exception -> ${e.localizedMessage}")
+            emit(errorHandler.handleHttpException(e))
+        } catch (e: IOException) {
+            Log.e(TAG, "exception -> ${e.localizedMessage}")
+            emit(Resource.Error(ErrorType.NO_CONNECTION))
+        } catch (e: JsonParseException) {
+            Log.e(TAG, "exception -> ${e.localizedMessage}")
+            emit(Resource.Error(ErrorType.UNEXPECTED))
+        } catch (e: SocketTimeoutException) {
+            Log.e(TAG, "exception -> ${e.localizedMessage}")
+            emit(Resource.Error(ErrorType.NO_CONNECTION))
+        } catch (e: Exception) {
+            Log.e(TAG, "exception -> ${e.localizedMessage}")
+            emit(Resource.Error(ErrorType.UNKNOWN_ERROR))
         }
     }
 
@@ -64,10 +82,13 @@ class FeedRepositoryImpl @Inject constructor(private val networkClient: NetworkC
                 }
             }
         }.getOrElse { exception ->
-            if (exception is SocketTimeoutException) {
-                Resource.Error(ErrorType.NO_CONNECTION)
-            } else {
-                Resource.Error(ErrorType.UNEXPECTED)
+            Log.e(TAG, "exception -> ${exception.localizedMessage}")
+            when(exception) {
+                is HttpException -> errorHandler.handleHttpException(exception)
+                is IOException -> Resource.Error(ErrorType.NO_CONNECTION)
+                is JsonParseException -> Resource.Error(ErrorType.UNEXPECTED)
+                is SocketTimeoutException -> Resource.Error(ErrorType.NO_CONNECTION)
+                else -> Resource.Error(ErrorType.UNEXPECTED)
             }
         }
     }
