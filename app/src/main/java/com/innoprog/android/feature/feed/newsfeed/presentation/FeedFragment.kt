5ec083f0e204ebe83d7de3d108e550241dca853a
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-07-11 01:17:58 +0300

    201_добавляет обработку ошибок в верстке

--- app/src/main/java/com/innoprog/android/feature/feed/newsfeed/presentation/FeedFragment.kt
+++ app/src/main/java/com/innoprog/android/feature/feed/newsfeed/presentation/FeedFragment.kt
@@ -8,6 +8,8 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
@@ -29,6 +31,7 @@ import com.innoprog.android.feature.feed.newsfeed.domain.models.NewsWithProject
 import com.innoprog.android.feature.feed.newsfeed.domain.models.PublicationType
 import com.innoprog.android.feature.newsrecycleview.NewsAdapter
 import com.innoprog.android.uikit.InnoProgChipGroupView
+import com.innoprog.android.util.ErrorScreenState
 import com.innoprog.android.util.debounceUnitFun
 
 class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
@@ -73,25 +76,25 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
         binding.rvPublications.layoutManager = LinearLayoutManager(context)
     }
 
-    private fun setUiListeners() {
-        binding.btnCreateIdea.setOnClickListener {
+    private fun setUiListeners() = with(binding) {
+        btnCreateIdea.setOnClickListener {
             Toast.makeText(requireContext(), "Переход на создание идеи", Toast.LENGTH_SHORT).show()
         }
 
-        binding.etSearch.setOnFocusChangeListener { _, _ ->
+        etSearch.setOnFocusChangeListener { _, _ ->
             startSearch()
         }
 
-        binding.tvCancel.setOnClickListener { cancelSearch() }
+        tvCancel.setOnClickListener { cancelSearch() }
 
-        binding.swipeRefreshLayout.setOnRefreshListener {
+        swipeRefreshLayout.setOnRefreshListener {
             newsAdapter.submitList(emptyList())
             viewModel.getNewsFeed()
         }
 
-        binding.etSearch.doOnTextChanged(textWatcherForEditText)
+        etSearch.doOnTextChanged(textWatcherForEditText)
 
-        binding.rvPublications.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        rvPublications.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if (dy > 0) {
@@ -109,6 +112,12 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
                 }
             }
         })
+
+        layoutErrorScreen.findViewById<com.innoprog.android.uikit.InnoProgButtonView>(
+            com.innoprog.android.uikit.R.id.ipbtn_repeat_request
+        ).setOnClickListener {
+            viewModel.getNewsFeed()
+        }
     }
 
     private fun initChips() {
@@ -132,7 +141,7 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
             }
 
             is FeedScreenState.Content -> showContent(state.newsFeed)
-            is FeedScreenState.Error -> showError(state.isPagination)
+            is FeedScreenState.Error -> renderError(state.errorType)
         }
     }
 
@@ -154,25 +163,78 @@ class FeedFragment : BaseFragment<FragmentFeedBinding, BaseViewModel>() {
         return (dp * context.resources.displayMetrics.density).toInt()
     }
 
-    private fun showLoading() {
-        Toast.makeText(requireContext(), "Загрузка", Toast.LENGTH_SHORT).show()
+    private fun showLoading() = with(binding) {
+        swipeRefreshLayout.isRefreshing = false
+
+        listOf(
+            tvPlaceholder,
+            layoutErrorScreen,
+            cgvFilter,
+            etSearch,
+            btnCreateIdea,
+            tvFeed,
+            btnCreateIdea,
+            rvPublications
+        ).forEach {
+            it.isVisible = false
+        }
+
+        circularProgress.isVisible = true
     }
 
-    private fun showError(isPagination: Boolean) {
-        if (isPagination) {
-            Toast.makeText(requireContext(), "FeedScreenState Ошибка пагинации", Toast.LENGTH_SHORT)
-                .show()
+    private fun renderError(errorState: ErrorScreenState) = with(binding) {
+        listOf(
+            tvPlaceholder,
+            cgvFilter,
+            etSearch,
+            btnCreateIdea,
+            tvFeed,
+            btnCreateIdea,
+            circularProgress
+        ).forEach {
+            it.isVisible = false
+        }
+        if (errorState == ErrorScreenState.UNAUTHORIZED) {
+            viewModel.clearBackStackAndNavigateToAuthorization()
         } else {
-            Toast.makeText(requireContext(), "FeedScreenState Ошибка", Toast.LENGTH_SHORT).show()
+            fetchErrorScreen(errorState)
+            layoutErrorScreen.isVisible = true
         }
     }
 
-    private fun showContent(newsFeed: List<NewsWithProject>) {
-        binding.apply {
-            binding.swipeRefreshLayout.isRefreshing = false
-            rvPublications.isVisible = true
-            newsAdapter.submitList(newsFeed)
+    private fun fetchErrorScreen(errorState: ErrorScreenState) {
+        val errorImageRes = errorState.imageResource
+        val errorTextRes = errorState.messageResource
+        binding.layoutErrorScreen.apply {
+            findViewById<ImageView>(com.innoprog.android.uikit.R.id.iv_error_image)
+                .setImageResource(errorImageRes)
+            findViewById<TextView>(com.innoprog.android.uikit.R.id.tv_error_message)
+                .setText(errorTextRes)
         }
+    }
+
+    private fun showContent(newsFeed: List<NewsWithProject>) = with(binding) {
+        swipeRefreshLayout.isRefreshing = false
+
+        listOf(
+            tvPlaceholder,
+            circularProgress,
+            layoutErrorScreen
+        ).forEach {
+            it.isVisible = false
+        }
+
+        listOf(
+            cgvFilter,
+            etSearch,
+            btnCreateIdea,
+            btnCreateIdea,
+            tvFeed,
+            rvPublications
+        ).forEach {
+            it.isVisible = true
+        }
+        newsAdapter.submitList(newsFeed)
     }
 
     @SuppressLint("ClickableViewAccessibility")
